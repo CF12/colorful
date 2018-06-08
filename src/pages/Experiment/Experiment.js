06684303facb8a3c023c8edceda085c4d4373f54
Brian X <CF12@users.noreply.github.com>
2018-06-08 10:07:02 -0400

    Work on scene Experiment

--- src/pages/Experiment/Experiment.js
+++ src/pages/Experiment/Experiment.js
@@ -2,11 +2,50 @@ import React from 'react'
 
 import './Experiment.css'
 
-export default class Experiment extends React.Component {
+const StateContext = new React.createContext(this.state)
+
+class Question1 extends React.PureComponent {
   render () {
     return (
       <div>
-        <p>Hello World!</p>
+        <p>What's your gender?</p>
+        <StateContext.Consumer>
+          <button onClick={() => { this }}
+        </StateContext.Consumer>
+      </div>
+    )
+  }
+}
+
+class Question2 extends React.PureComponent {
+  render() {
+    return (
+      <p>What's your favorite color?</p>
+    )
+  }
+}
+
+const questions = {
+  1: <Question1 />,
+  2: <Question2 />
+}
+
+export default class Experiment extends React.PureComponent {
+  constructor () {
+    super()
+
+    this.state = {
+      question: 1
+    }
+  }
+
+  render () {
+
+    return (
+      <div>
+        <StateContext.Provider>
+          { questions[this.state.question] }
+        </StateContext.Provider>
       </div>
     )
   }
