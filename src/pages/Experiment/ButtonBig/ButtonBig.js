3b251209db70438e9d848d2b14c2d970c71c1c5d
Brian X <brianx0011@gmail.com>
2018-06-10 22:10:06 -0400

    Major design overhaul on questions, and completed button state system

---
+++ src/pages/Experiment/ButtonBig/ButtonBig.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './ButtonBig.css'
+
+export default class ButtonBig extends React.PureComponent {
+  constructor (props) {
+    super()
+
+    this.buttonKey = props.buttonKey
+    this.text = props.text
+    this.iconName = props.iconName
+    this.handler = props.handler
+    this.valueKey = props.valueKey
+    this.value = props.value
+    this.active = props.active
+  }
+
+  componentWillReceiveProps (newProps) {
+    this.active = newProps.active
+  }
+
+  render () {
+    return (
+      <div
+        className={'flex--center flex--column button--big ' + (this.active ? 'button--big__active' : '')}
+        onClick={() => { this.handler(this.buttonKey, this.value) }}>
+        <p>{this.text}</p>
+      </div>
+    )
+  }
+}
--- src/pages/Experiment/Experiment.js
+++ src/pages/Experiment/Experiment.js
@@ -2,15 +2,78 @@ import React from 'react'
 
 import './Experiment.css'
 
-const StateContext = new React.createContext()
+import ButtonBig from './ButtonBig/ButtonBig'
+
+const StateContext = React.createContext()
 
 class Question1 extends React.PureComponent {
+  constructor (props) {
+    super()
+
+    this.buttons = [
+      {
+        text: 'Male',
+        value: 'male',
+        iconName: ''
+      },
+      {
+        text: 'Female',
+        value: 'female',
+        iconName: ''
+      },
+      {
+        text: 'Other / Don\'t specify',
+        value: 'other',
+        iconName: ''
+      }
+    ]
+
+    this.state = {
+      active: -1,
+      gender: ''
+    }
+  }
+
+  getState () {
+    return this.state
+  }
+
+  clickHandler (key, value) {
+    this.setState({
+      active: key,
+      gender: value
+    })
+  }
+
   render (props) {
     return (
       <div>
         <p>What's your gender?</p>
+
+        <div className='flex--row'>
+          {
+            this.buttons.map((value, key) => {
+              return <ButtonBig
+                key={key}
+                buttonKey={key}
+                text={value.text}
+                value={value.value}
+                iconName={value.iconName}
+                active={this.state.active === key}
+                handler={(key, value) => { this.clickHandler(key, value) }}
+              />
+            })
+          }
+        </div>
+
         <StateContext.Consumer>
-          { state => <button onClick={() => { state.setQuestion(2) }}>Hello!</button> }
+          {
+            state => {
+              return (
+                <button onClick={state.nextQuestion}>Hello!</button>
+              )
+            }
+          }
         </StateContext.Consumer>
       </div>
     )
@@ -25,18 +88,27 @@ class Question2 extends React.PureComponent {
   }
 }
 
-const questions = {
-  1: <Question1 />,
-  2: <Question2 />
-}
-
 export default class Experiment extends React.PureComponent {
   constructor () {
     super()
 
+    this.questions = {
+      1: <Question1 />,
+      2: <Question2 />
+    }
+
     this.state = {
       question: 1,
-      setQuestion: (value) => this.setState({ question: value })
+      nextQuestion: (value) => this.setState({
+        question: (this.state.question + 1 in this.questions)
+          ? this.state.question + 1
+          : this.state.question
+      }),
+      prevQuestion: (value) => this.setState({
+        question: (this.state.question - 1 in this.questions)
+          ? this.state.question - 1
+          : this.state.question
+      })
     }
   }
 
@@ -45,9 +117,17 @@ export default class Experiment extends React.PureComponent {
       <div className='experiment flex--center'>
         <div className='experiment__wrapper flex--center'>
           <StateContext.Provider value={this.state}>
-            <Question1 />
             <StateContext.Consumer>
-              { state => <p> {state.question} </p> }
+              {
+                state => {
+                  return (
+                    <div>
+                      {this.questions[state.question]}
+                      <p> {state.question} </p>
+                    </div>
+                  )
+                }
+              }
             </StateContext.Consumer>
           </StateContext.Provider>
         </div>
